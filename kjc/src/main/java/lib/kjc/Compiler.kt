ad15db580ebb500a0ba26fa9a408cb2e5e17d331
kotleni <kotleni@Victors-MacBook-Air.local>
2022-06-01 18:32:20 +0300

    Compiler has ben commented (for future)

--- kjc/src/main/java/lib/kjc/Compiler.kt
+++ kjc/src/main/java/lib/kjc/Compiler.kt
@@ -5,25 +5,26 @@ import java.io.PrintWriter
 import java.io.StringWriter
 
 class JavaCompiler {
-    fun compileProject(projectDirPath: String) {
-        val _out = StringWriter()
-        val _err = StringWriter()
-        val compiler = Main(
-            PrintWriter(_out),
-            PrintWriter(_err),
-            false,
-            null,
-            null)
-        val args = mutableListOf<String>()
-        args.addAll(listOf(
-            // "-extdirs", libsFolder,
-            // "-bootclasspat", androidJar
-            "-1.7",
-            "-proc:none",
-            "-target", "1.7",
-            "-d $projectDirPath/build/classes/",
-            "$projectDirPath/src/"
-        ))
-        compiler.compile(args.toTypedArray())
-    }
+    // in future
+//    fun compileProject(projectDirPath: String) {
+//        val _out = StringWriter()
+//        val _err = StringWriter()
+//        val compiler = Main(
+//            PrintWriter(_out),
+//            PrintWriter(_err),
+//            false,
+//            null,
+//            null)
+//        val args = mutableListOf<String>()
+//        args.addAll(listOf(
+//            // "-extdirs", libsFolder,
+//            // "-bootclasspat", androidJar
+//            "-1.7",
+//            "-proc:none",
+//            "-target", "1.7",
+//            "-d $projectDirPath/build/classes/",
+//            "$projectDirPath/src/"
+//        ))
+//        compiler.compile(args.toTypedArray())
+//    }
 }
